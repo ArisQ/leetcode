13fd2dd667b9bf22a8747bf49eb5c2d72d74e2d0
ArisQ <qiaoli.pg@gmail.com>
2019-08-11 10:52:38 +0800

    86. Partition List

--- 86-partition-list/main.cc
+++ 86-partition-list/main.cc
@@ -7,29 +7,37 @@ using namespace std;
 
 class Solution {
 public:
-    ListNode* partition(ListNode* head,int x){
-        ListNode dummy(0);
-        ListNode *lessEnd=&dummy,*currentLead=&dummy,*current=nullpt;
-        dummy.next=head;
-        while(currentLead->next!=nullptr){
-//            if(currentLead->next->val<x){
-//                current=currentLead->next;
-//                currentLead->next=current->next;
-//                current->next=lessEnd->next;
-//                lessEnd->next=current;
-//                lessEnd=current;
-//            }
-            currentLead=currentLead->next;
+    ListNode *partition(ListNode *head, int x) {
+        ListNode lessHead(0), greatEqualHead(0);
+        ListNode *lessEnd = &lessHead, *greatEqualEnd = &greatEqualHead;
+        while (head != nullptr) {
+            if (head->val < x) {
+                lessEnd->next = head;
+                lessEnd = head;
+            } else {
+                greatEqualEnd->next = head;
+                greatEqualEnd = head;
+            }
+            head = head->next;
         }
-        cout<<dummy.next;
-        return dummy.next;
+        lessEnd->next = greatEqualHead.next;
+        greatEqualEnd->next = nullptr;
+        return lessHead.next;
     }
 };
 
 TEST_CASE("Partition List") {
-    ListNode *input=makeList({1,4,3,2,5,2});
-    ListNode *output=makeList({1,2,2,4,3,5});
-    cout<<input;
-    cout<<output;
-    REQUIRE(equalList(Solution().partition(input,3),output));
+    ListNode *input = makeList({1, 4, 3, 2, 5, 2});
+    ListNode *output = makeList({1, 2, 2, 4, 3, 5});
+    cout << input << endl;
+    cout << output << endl;
+    REQUIRE(equalList(Solution().partition(input, 3), output));
+}
+
+TEST_CASE("Partition List - Wrong Answer 1") {
+    ListNode *input = makeList({3, 1, 2});
+    ListNode *output = makeList({1, 2, 3});
+    cout << input << endl;
+    cout << output << endl;
+    REQUIRE(equalList(Solution().partition(input, 3), output));
 }
